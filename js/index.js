4a54756978801205e538496fbb21e3ce6d77ee35
LadyKerr <kadiekerr@gmail.com>
2019-05-04 08:04:51 -0400

    added event listeners to btn and logo

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
 // Your code goes here
+const logo = document.querySelector('.logo-heading');
+console.log(logo);
+logo.addEventListener('click', (event) => {
+  logo.style.color = 'red';
+  logo.style.fontSize = '60px';
+  });
+
+  //On click buttons change
+  const btn = document.querySelectorAll('.btn');
+    console.log(btn);
+  btn.forEach((button) => {
+    button.addEventListener('click', (event) => {
+    button.style.backgroundColor = 'yellow';
+    button.style.color = 'black';
+    alert('Thank you for choosing FunBus!');
+    });
+  });
+
